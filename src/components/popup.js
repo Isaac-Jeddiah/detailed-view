7c395f3d581a4fae6dbd539a47ed905343bdb966
Isaac Jeddiah <isaacjeddiah@gmail.com>
2025-02-06 10:55:02 +0530

    50

--- src/components/popup.js
+++ src/components/popup.js
@@ -236,7 +236,9 @@ const Popup = ({ closePopup }) => {
               defaultValue="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
               size="small"
               fullWidth
-              
+              value={formValues.bio}
+                onChange={(e) => handleTextChange('bio', )}
+                
               multiline
               rows={4}
               InputProps={{
