5885e45fccab61e54b8c5ec250c82c497c363272
ISAAC JEDDIAH <121207769+Isaac-Jeddiah@users.noreply.github.com>
2025-02-26 12:39:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
 import Dashboard1 from './pages/dashboard1';
+import Dashboard2 from './pages/Listview';
 import Gridlayout from './pages/gridBox';
 import { ThemeProvider } from '@mui/material/styles';
 import DetailsTable from './components/details';
